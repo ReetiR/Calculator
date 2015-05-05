2c219e4dacbca971d963e7b0bece1c184e4a3c6e
Reetika Roy <royritu21@yahoo.co.in>
2015-05-05 23:17:49 +0530

    Simple calculator using C#

---
+++ Calculator/Calculator/Cal.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Cal.aspx.cs" Inherits="Calculator.Cal" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title> Calculatore</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <table border="3" style="height:300px; width:200px; margin-left:550px;">
+      <tr>
+      <td colspan="4"><asp:TextBox BorderWidth="2" BackColor="Black" ID="text_result" Width="215" Height="60" runat="server" ForeColor="White" Font-Size="20"></asp:TextBox></td>
+      </tr>
+      <tr>
+      <td><asp:Button ID="bttn1" Width="50" Height="60" runat="server" Text="1" Font-Size="16" onclick="bttn1_Click" /></td>
+      <td><asp:Button ID="bttn2" Width="50" Height="60" runat="server" Text="2" Font-Size="16" onclick="bttn2_Click" /></td>
+      <td><asp:Button ID="bttn3" Width="50" Height="60" runat="server" Text="3" Font-Size="16" onclick="bttn3_Click" /></td>
+      <td><asp:Button ID="bttn_add" Width="50" Height="60" runat="server" Text="+" Font-Size="16" onclick="bttn_add_Click" /></td>
+      </tr>
+      <tr>
+      <td><asp:Button ID="bttn4" Width="50" Height="60" runat="server" Text="4" Font-Size="16" onclick="bttn4_Click" /></td>
+      <td><asp:Button ID="bttn5" Width="50" Height="60" runat="server" Text="5" Font-Size="16" onclick="bttn5_Click" /></td>
+      <td><asp:Button ID="bttn6" Width="50" Height="60" runat="server" Text="6" Font-Size="16" onclick="bttn6_Click" /></td>
+      <td><asp:Button ID="bttn_sub" Width="50" Height="60" runat="server" Text="-" Font-Size="16" onclick="bttn_sub_Click" /></td>
+      </tr>
+      <tr>
+      <td><asp:Button ID="bttn7" Width="50" Height="60" runat="server" Text="7" Font-Size="16" onclick="bttn7_Click" /></td>
+      <td><asp:Button ID="bttn8" Width="50" Height="60" runat="server" Text="8" Font-Size="16" onclick="bttn8_Click" /></td>
+      <td><asp:Button ID="bttn9" Width="50" Height="60" runat="server" Text="9" Font-Size="16" onclick="bttn9_Click" /></td>
+      <td><asp:Button ID="bttn_prod" Width="50" Height="60" runat="server" Text="*" Font-Size="16" onclick="bttn_prod_Click" /></td>
+      </tr>
+      <tr>
+      <td><asp:Button ID="bttn0" Width="50" Height="60" runat="server" Text="0" Font-Size="16" onclick="bttn0_Click" /></td>
+      <td><asp:Button ID="bttn_clr" Width="50" Height="60" runat="server" Text="C" Font-Size="16" onclick="bttn_clr_Click" /></td>
+      <td><asp:Button ID="bttn_eq" Width="50" Height="60" runat="server" Text="=" Font-Size="16" onclick="bttn_eq_Click" /></td>
+      <td><asp:Button ID="bttn_div" Width="50" Height="60" runat="server" Text="/" Font-Size="16" onclick="bttn_div_Click" /></td>
+      </tr>
+    </table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Calculator/Calculator/Cal.aspx.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calculator
+{
+    public partial class Cal : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void bttn1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(text_result.Text))
+                text_result.Text = bttn1.Text;
+            else
+                text_result.Text = text_result.Text + bttn1.Text;
+        }
+
+        protected void bttn2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(text_result.Text))
+                text_result.Text = bttn2.Text;
+            else
+                text_result.Text = text_result.Text + bttn2.Text;
+        }
+
+        protected void bttn3_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(text_result.Text))
+                text_result.Text = bttn3.Text;
+            else
+                text_result.Text = text_result.Text + bttn3.Text;
+        }
+
+        protected void bttn4_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(text_result.Text))
+                text_result.Text = bttn4.Text;
+            else
+                text_result.Text = text_result.Text + bttn4.Text;
+        }
+
+        protected void bttn5_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(text_result.Text))
+                text_result.Text = bttn5.Text;
+            else
+                text_result.Text = text_result.Text + bttn5.Text;
+        }
+
+        protected void bttn6_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(text_result.Text))
+                text_result.Text = bttn6.Text;
+            else
+                text_result.Text = text_result.Text + bttn6.Text;
+        }
+
+        protected void bttn7_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(text_result.Text))
+                text_result.Text = bttn7.Text;
+            else
+                text_result.Text = text_result.Text + bttn7.Text;
+        }
+
+        protected void bttn8_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(text_result.Text))
+                text_result.Text = bttn8.Text;
+            else
+                text_result.Text = text_result.Text + bttn8.Text;
+        }
+
+        protected void bttn9_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(text_result.Text))
+                text_result.Text = bttn9.Text;
+            else
+                text_result.Text = text_result.Text + bttn9.Text;
+        }
+
+        protected void bttn0_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(text_result.Text))
+                text_result.Text = bttn0.Text;
+            else
+                text_result.Text = text_result.Text + bttn0.Text;
+        }
+
+        protected void bttn_add_Click(object sender, EventArgs e)
+        {
+            ViewState["CurrentValue"] = text_result.Text;
+            ViewState["CurrentOperator"] = bttn_add.Text;
+            text_result.Text = "";
+        }
+
+        protected void bttn_sub_Click(object sender, EventArgs e)
+        {
+            ViewState["CurrentValue"] = text_result.Text;
+            ViewState["CurrentOperator"] = bttn_sub.Text;
+            text_result.Text = "";
+
+
+        }
+
+        protected void bttn_prod_Click(object sender, EventArgs e)
+        {
+            ViewState["CurrentValue"] = text_result.Text;
+            ViewState["CurrentOperator"] = bttn_prod.Text;
+            text_result.Text = "";
+
+
+        }
+
+        protected void bttn_div_Click(object sender, EventArgs e)
+        {
+            ViewState["CurrentValue"] = text_result.Text;
+            ViewState["CurrentOperator"] = bttn_div.Text;
+            text_result.Text = "";
+
+
+        }
+
+        protected void bttn_eq_Click(object sender, EventArgs e)
+        {
+            if (ViewState["CurrentOperator"].ToString() == "+")
+            {
+                text_result.Text = Convert.ToString(Convert.ToInt32(ViewState["CurrentValue"].ToString()) + Convert.ToInt32(text_result.Text));
+            }
+            else if(ViewState["CurrentOperator"].ToString() == "-")
+            {
+                text_result.Text = Convert.ToString(Convert.ToInt32(ViewState["CurrentValue"].ToString()) - Convert.ToInt32(text_result.Text));
+            }
+            else if (ViewState["CurrentOperator"].ToString() == "*")
+            {
+                text_result.Text = Convert.ToString(Convert.ToInt32(ViewState["CurrentValue"].ToString()) * Convert.ToInt32(text_result.Text));
+            }
+            else
+            {
+                text_result.Text = Convert.ToString(Convert.ToInt32(ViewState["CurrentValue"].ToString()) / Convert.ToInt32(text_result.Text));
+            }
+        }
+
+        protected void bttn_clr_Click(object sender, EventArgs e)
+        {
+            text_result.Text = "";
+        }
+    }
+}
